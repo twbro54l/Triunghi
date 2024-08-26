7dafb1e67bd894b5a8056ec5c46437ed57742c7f
botez <botez@DAVID>
2024-08-26 21:18:58 +0300

    Add project files.

---
+++ Triunghi/Triunghi.cpp
@@ -0,0 +1,27 @@
+﻿#include "Triunghi.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	unsigned int x, x0, x1, x2, x3;
+	cin >> x;
+
+	x0 = x % 10; // reținem cifra unităților
+	x = x / 10;  // eliminăm cifra unităților
+
+	x1 = x % 10; // reținem cifra zecilor
+	x = x / 10;  // eliminăm cifra zecilor
+
+	x2 = x % 10; // reținem cifra sutelor
+	x = x / 10;  // eliminăm cifra sutelor
+
+	x3 = x % 10; // reținem cifra miilor
+	x = x / 10;  // în x rămâne cifra zecilor de mii
+
+	cout << "  " << x2 << endl;
+	cout << " " << x3 << x2 << x1 << endl;
+	cout << x << x3 << x2 << x1 << x0 << endl;
+
+	return 0;
+}
